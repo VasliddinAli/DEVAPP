3f3156dfa00de6ea9c5101d42fa2df350b94d804
VasliddinAli <vasliddinali@gmail.com>
2023-04-30 08:45:42 +0500

    DEVAPP 2

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -706,38 +706,38 @@
 
 
 	<!-- jquery include -->
-	<script src="assets/js/jquery-3.4.1.min.js"></script>
-	<script src="assets/js/popper.min.js"></script>
-	<script src="assets/js/bootstrap.min.js"></script>
+	<script src="{{ asset('assets/js/jquery-3.4.1.min.js') }}"></script>
+	<script src="{{ asset('assets/js/popper.min.js') }}"></script>
+	<script src="{{ asset('assets/js/bootstrap.min.js') }}"></script>
 
 	<!-- slider & carousel - jquery include -->
-	<script src="assets/js/owl.carousel.min.js"></script>
+	<script src="{{ asset('assets/js/owl.carousel.min.js') }}"></script>
 
 	<!-- animation - jquery include -->
 	<!-- <script src="assets/js/aos.js"></script> -->
-	<script src="assets/js/splitting.js"></script>
+	<script src="{{ asset('assets/js/splitting.js') }}"></script>
 
 	<!-- magnific popup - jquery include -->
-	<script src="assets/js/magnific-popup.min.js"></script>
+	<script src="{{ asset('assets/js/magnific-popup.min.js') }}"></script>
 
 	<!-- isotope filter - jquery include -->
-	<script src="assets/js/isotope.pkgd.min.js"></script>
-	<script src="assets/js/masonry.pkgd.min.js"></script>
-	<script src="assets/js/imagesloaded.pkgd.min.js"></script>
+	<script src="{{ asset('assets/js/isotope.pkgd.min.js') }}"></script>
+	<script src="{{ asset('assets/js/masonry.pkgd.min.js') }}"></script>
+	<script src="{{ asset('assets/js/imagesloaded.pkgd.min.js') }}"></script>
 
 	<!-- mouse move & scroll parallax  - jquery include -->
-	<script src="assets/js/parallax.min.js"></script>
-	<script src="assets/js/parallax-scroll.js"></script>
+	<script src="{{ asset('assets/js/parallax.min.js') }}"></script>
+	<script src="{{ asset('assets/js/parallax-scroll.js') }}"></script>
 
 	<!-- google map - jquery include -->
 	<script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyAhrdEzlfpnsnfq4MgU1e1CCsrvVx2d59s"></script>
-	<script src="assets/js/gmaps.js"></script>
+	<script src="{{ asset('assets/js/gmaps.js') }}"></script>
 
 	<!-- mobile menu - jquery include -->
-	<script src="assets/js/mCustomScrollbar.js"></script>
+	<script src="{{ asset('assets/js/mCustomScrollbar.js') }}"></script>
 
 	<!-- custom - jquery include -->
-	<script src="assets/js/custom.js"></script>
+	<script src="{{ asset('assets/js/custom.js') }}"></script>
 
 
 </body>
